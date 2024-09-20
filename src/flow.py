e5268d32b139c80c6b7c825d98c077c2b8f65a16
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-09-20 16:12:11 +0100

    be able to thin the gradient flow data

--- src/flow.py
+++ src/flow.py
@@ -10,6 +10,9 @@ def get_args():
 
     parser.add_argument("flow_filename", help="Filename of flow log to analyse")
     parser.add_argument("--filetype", choices=list(readers), default="hirep", help="How to interpret the input file")
+    parser.add_argument("--min_trajectory", type=int, default=None, help="Lowest trajectory index to consider")
+    parser.add_argument("--max_trajectory", type=int, default=None, help="Highest trajectory index to consider")
+    parser.add_argument("--trajectory_step", type=int, default=1, help="Interval of trajectories to consider")
     parser.add_argument("W0", type=float, help="Threshold value at which to take t = w0^2")
     parser.add_argument("--operator", choices=["plaq", "sym"], default="sym", help="Flow operator to use")
     parser.add_argument("--output_file_mean", type=FileType("w"), default="-", help="Where to output the mean and uncertainty of w0")
@@ -21,7 +24,11 @@ def get_args():
 def main():
     args = get_args()
 
-    flows = readers[args.filetype](args.flow_filename)
+    flows = readers[args.filetype](args.flow_filename).thin(
+        min_trajectory=args.min_trajectory,
+        max_trajectory=args.max_trajectory,
+        trajectory_step=args.trajectory_step,
+    )
     w0_samples = bootstrap_ensemble_w0(flows, args.W0, operator=args.operator)
     w0_mean = bootstrap_finalize(w0_samples)
 
