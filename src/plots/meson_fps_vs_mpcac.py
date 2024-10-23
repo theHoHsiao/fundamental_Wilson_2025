1dc2e8527e4368db65af702f9f4de24beb93fa87
Ho Hsiao <162536018+theHoHsiao@users.noreply.github.com>
2024-10-23 16:39:20 +0900

    Create meson_fps_vs_mpcac.py

---
+++ src/plots/meson_fps_vs_mpcac.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from ..plots_common import standard_plot_main, channel_color
+from ..mass import C_R
+
+
+def plot(data):
+    fig, ax = plt.subplots(1, 1, num="Figure_10", figsize=(6, 4), layout="constrained")
+
+    ax.set_ylim(5, 11)
+    ax.set_xlim(0, 0.18)
+    ax.set_xlabel(r"$am_{\rm PCAC}$")
+    ax.set_ylabel(r"$m_{\rm M} / f_{\rm ps}$")
+
+    channels = ["ps", "v", "av", "at", "s"]
+    markers = "o^sx+"
+
+    for channel_idx, (channel, marker) in enumerate(zip(channels, markers)):
+        to_plot = []
+
+        for datum in data:
+            if "mPCAC_samples" not in datum:
+                continue
+
+            if f"{channel}_mass_samples" not in datum:
+                continue
+
+            Z_factor = 1 + 2 * (C_R("ps")) * (8 / datum["beta"]) / (
+                16 * 3.141592653589793**2 * datum["plaquette_samples"].samples
+            )
+
+            X = datum["mPCAC_samples"].samples
+            Y = (
+                datum[f"{channel}_mass_samples"].samples
+                / datum["ps_matrix_element_samples"].samples
+                / Z_factor
+            )
+
+            to_plot.append((Y.mean(), Y.std(), X.mean(), X.std()))
+
+        y_values, y_errors, x_values, x_errors = zip(*to_plot)
+
+        ax.errorbar(
+            x_values,
+            y_values,
+            xerr=x_errors,
+            yerr=y_errors,
+            ls="none",
+            alpha=0.7,
+            color=channel_color(channel),
+            label=channel,
+            marker=marker,
+        )
+
+    handles, labels = fig.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    fig.legend(
+        by_label.values(),
+        by_label.keys(),
+        loc="outside upper center",
+        ncol=6,
+        borderaxespad=0.2,
+    )
+
+    return fig
+
+
+if __name__ == "__main__":
+    standard_plot_main(plot)
