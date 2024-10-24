f3259a417484ee9454e7393440e9ac614d19b7b2
Ho Hsiao <162536018+theHoHsiao@users.noreply.github.com>
2024-10-24 15:42:07 +0900

    Create gmor.py

---
+++ src/plots/gmor.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from ..plots_common import standard_plot_main
+from ..mass import C_R
+
+
+def plot(data):
+    fig, ax = plt.subplots(1, 1, num="Figure_20", figsize=(6, 4), layout="constrained")
+
+    ax.set_xlim(0, 0.16)
+    ax.set_xlabel(r"$am_{\rm PCAC}$")
+    ax.set_ylabel(r"$(af_{\rm ps})^2  (am_{\rm ps})^2$")
+
+    to_plot = []
+
+    for datum in data:
+        if "ps_mass_samples" not in datum:
+            continue
+        if "mPCAC_samples" not in datum:
+            continue
+
+        Z_factor = 1 + 2 * (C_R("ps")) * (8 / datum["beta"]) / (
+            16 * 3.141592653589793**2 * datum["plaquette_samples"].samples
+        )
+
+        X = datum["mPCAC_samples"].samples
+        Y = (
+            datum["ps_mass_samples"].samples
+            * datum["ps_matrix_element_samples"].samples
+            * Z_factor
+        ) ** 2
+
+        to_plot.append((Y.mean(), Y.std(), X.mean(), X.std()))
+
+    y_values, y_errors, x_values, x_errors = zip(*to_plot)
+
+    # print(np.array(to_fit_x).shape)
+
+    ax.errorbar(
+        x_values,
+        y_values,
+        xerr=x_errors,
+        yerr=y_errors,
+        ls="none",
+        alpha=0.7,
+        color="b",
+        marker="s",
+    )
+
+    return fig
+
+
+if __name__ == "__main__":
+    standard_plot_main(plot)
