f0205c4897e84649445aa3009a8f9d1442b6087e
Ho Hsiao <162536018+theHoHsiao@users.noreply.github.com>
2024-10-27 00:04:53 +0900

    calculate decay constant
    move decay constant outside plots

---
+++ src/decay_constant.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import numpy as np
+from argparse import ArgumentParser, FileType
+from uncertainties import ufloat
+
+from .dump import read_sample_files, dump_dict, dump_samples
+from .mass import C_R
+
+
+def get_args():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "data_filename",
+        nargs="+",
+        metavar="sample_filename",
+        help="Filename of a sample file",
+    )
+    parser.add_argument(
+        "--channel",
+        choices=["ps", "v", "av"],
+        default=None,
+        help="Measuring channel",
+    )
+    parser.add_argument(
+        "--output_file_mean",
+        type=FileType("w"),
+        default="-",
+        help="Where to output the mean and uncertainty",
+    )
+    parser.add_argument(
+        "--output_file_samples",
+        type=FileType("w"),
+        default=None,
+        help="Where to output the bootstrap samples",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    datum = read_sample_files(args.data_filename)[0]
+
+    Z_factor = 1 + 2 * (C_R(args.channel)) * (8 / datum["beta"]) / (
+        16 * np.pi**2 * datum["plaquette_samples"].samples
+    )
+
+    Z_factor_mean = 1 + 2 * (C_R(args.channel)) * (8 / datum["beta"]) / (
+        16 * np.pi**2 * datum["plaquette_samples"].mean
+    )
+
+    decay_constant_sample = (
+        datum[f"{args.channel}_matrix_element_samples"].samples * Z_factor
+    )
+    decay_constant_mean = (
+        datum[f"{args.channel}_matrix_element_samples"].mean * Z_factor_mean
+    )
+
+    decay_constant = ufloat(decay_constant_mean, decay_constant_sample.std())
+
+    metadata = {
+        "ensemble_name": datum["ensemble_name"],
+        "beta": datum["beta"],
+        "mAS": datum["mAS"],
+        "Nt": datum["Nt"],
+        "Ns": datum["Ns"],
+    }
+    dump_dict(
+        {
+            **metadata,
+            f"{args.channel}_decay_constant": decay_constant,
+        },
+        args.output_file_mean,
+    )
+    if args.output_file_samples:
+        dump_samples(
+            {
+                **metadata,
+                f"{args.channel}_decay_constant_samples": decay_constant_sample,
+                f"{args.channel}_decay_constant_value": decay_constant_mean,
+            },
+            args.output_file_samples,
+        )
+
+
+if __name__ == "__main__":
+    main()
--- workflow/rules/mass.smk
+++ workflow/rules/mass.smk
@@ -61,6 +61,26 @@ rule fit_mass_GEVP: # for rhoE1 only
     shell:
         "python -m {params.module} {input.data} --output_file_mean {output.mean} --output_file_samples {output.samples} --ensemble_name {params.metadata.ensemble_name} --beta {params.metadata.beta} --mAS {params.metadata.mAS} --Nt {params.metadata.Nt} --Ns {params.metadata.Ns} --plateau_start {params.metadata.smear_rhoE1_plateau_start} --plateau_end {params.metadata.smear_rhoE1_plateau_end} --min_trajectory {params.metadata.init_conf} --max_trajectory {params.metadata.final_conf} --trajectory_step {params.metadata.delta_conf_spectrum} --N_sink {params.metadata.rhoE1_N_sink} --num_source {params.metadata.smear_num_source} --epsilon {params.metadata.smear_epsilon} --GEVP_t0 {params.metadata.GEVP_t0}"
 
+def mass_samples(wildcards):
+    return [
+        f"intermediary_data/{dir_template}/meson_{wildcards.channel}_samples.json",
+        f"intermediary_data/{dir_template}/plaquette_samples.json"
+    ]
+
+rule get_meson_decay:
+    params:
+        module=lambda wildcards, input: input.script.replace("/", ".")[:-3],
+    input:
+        data=mass_samples,
+        script="src/decay_constant.py",
+    output:
+        mean=f"intermediary_data/{dir_template}/decay_constant_{{channel}}_mean.csv",
+        samples=f"intermediary_data/{dir_template}/decay_constant_{{channel}}_samples.json",
+    conda:
+        "../envs/flow_analysis.yml"
+    shell:
+        "python -m {params.module} {input.data} --channel {wildcards.channel} --output_file_mean {output.mean} --output_file_samples {output.samples}"
+
 def all_mass_data(wildcards):
     return [
         f"intermediary_data/{dir_template}/meson_{channel}_mean.csv".format(channel=channel, **row)
