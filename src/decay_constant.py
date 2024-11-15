1805722a7a536fe0816f8dd359f080ff48ea1dae
Ho Hsiao <162536018+theHoHsiao@users.noreply.github.com>
2024-11-15 12:00:46 +0900

    update for BootstrapSampleSet

--- src/decay_constant.py
+++ src/decay_constant.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
 from argparse import ArgumentParser, FileType
-from uncertainties import ufloat
 
 from .dump import read_sample_files, dump_dict, dump_samples
 from .mass import renormalisation_constant
@@ -43,22 +42,12 @@ def main():
 
     datum = read_sample_files(args.data_filename)[0]
 
+    #  calculation of renormalisation factor based on Eqs. (29) and (30)
     Z_factor = 1 + 2 * (renormalisation_constant(args.channel)) * (
         8 / datum["beta"]
-    ) / (16 * np.pi**2 * datum["plaquette_samples"].samples)
+    ) / (16 * np.pi**2 * datum["plaquette_samples"])
 
-    Z_factor_mean = 1 + 2 * (renormalisation_constant(args.channel)) * (
-        8 / datum["beta"]
-    ) / (16 * np.pi**2 * datum["plaquette_samples"].mean)
-
-    decay_constant_sample = (
-        datum[f"{args.channel}_matrix_element_samples"].samples * Z_factor
-    )
-    decay_constant_mean = (
-        datum[f"{args.channel}_matrix_element_samples"].mean * Z_factor_mean
-    )
-
-    decay_constant = ufloat(decay_constant_mean, decay_constant_sample.std())
+    decay_constant = datum[f"{args.channel}_matrix_element_samples"] * Z_factor
 
     metadata = {
         "ensemble_name": datum["ensemble_name"],
@@ -70,7 +59,7 @@ def main():
     dump_dict(
         {
             **metadata,
-            f"{args.channel}_decay_constant": decay_constant,
+            f"{args.channel}_decay_constant": decay_constant.to_ufloat(),
         },
         args.output_file_mean,
     )
@@ -78,8 +67,7 @@ def main():
         dump_samples(
             {
                 **metadata,
-                f"{args.channel}_decay_constant_samples": decay_constant_sample,
-                f"{args.channel}_decay_constant_value": decay_constant_mean,
+                f"{args.channel}_decay_constant_samples": decay_constant,
             },
             args.output_file_samples,
         )
