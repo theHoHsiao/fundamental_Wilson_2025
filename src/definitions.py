ac74d386310e5100e709acf003cdaa5fe06b58c5
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-11-29 09:50:14 +0000

    add provenance stamps to definition file outputs

--- src/definitions.py
+++ src/definitions.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+from .provenance import get_basic_metadata, text_metadata
+
 
 def format_definitions(definitions):
-    formatted_definitions = [
-        r"\newcommand{{\{}}}{{{}}}".format(name, value)
-        for name, value in definitions.items()
-    ]
+    formatted_definitions = [text_metadata(get_basic_metadata(), comment_char="%")]
+    formatted_definitions.extend(
+        [
+            r"\newcommand{{\{}}}{{{}}}".format(name, value)
+            for name, value in definitions.items()
+        ]
+    )
     return "\n".join(formatted_definitions)
